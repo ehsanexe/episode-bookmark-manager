e9feb9c6296366ce6ab2856656db442b1293314a
Technomites <technomites@Technomitess-MacBook-Pro.local>
2022-02-15 11:17:40 +0500

    dynamic inputs

--- popup.js
+++ popup.js
@@ -1,20 +1,23 @@
-let x = false;
+let ids = [];
 
 document.getElementById("add").onclick = function handleAdd(e) {
-  // Math.random()
+  let id = Math.floor(Math.random() * 1000000);
+  ids.push(id);
+
   const div = document.createElement(`div`);
-  div.innerHTML = `<input id="50" placeholder='Enter show name' /> <button id="btn" >Save</button> `;
+  div.innerHTML = `<input id="i${id}" placeholder='Enter show name' />`;
   document.getElementById("shows").appendChild(div);
-  x = true;
 };
 
 // function handleSave() {
 //   console.log("z");
 // }
 
-if (x) {
-  document.getElementById("btn").onclick = function handleSave(e) {
-    //   let x = document.getElementById("50").value;
-    console.log("sss");
-  };
-}
+document.getElementById("save").onclick = function handleSave(e) {
+  //   let x = document.getElementById("50").value;
+  let shows = [];
+  for (let i = 0; i < ids.length; i++) {
+    shows.push(document.getElementById(`i${ids[i]}`).value);
+  }
+  console.log("shows", shows);
+};
