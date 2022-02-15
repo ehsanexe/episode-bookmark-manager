7ca2bfae702cf4d4a04163237ec221275182ad81
Technomites <technomites@Technomitess-MacBook-Pro.local>
2022-02-15 16:55:10 +0500

    working finally

--- background.js
+++ background.js
@@ -3,55 +3,55 @@ chrome.history.onVisited.addListener(async (res) => {
 
   chrome.storage.sync.get(["shows"], function (result) {
     // console.log("Value currently is " + result.shows);
-    let arr = JSON.parse(result.shows);
+    let shows = JSON.parse(result.shows);
 
-    arr.forEach((element) => {
+    shows.forEach((show) => {
       if (
-        res.title.includes(element.title) &&
+        res.title.includes(show.title) &&
         res.title.toLowerCase().includes("episode") &&
         !res.title.toLowerCase().includes("youtube")
       ) {
-        console.log("yay");
-        return;
-      }
-    });
-  });
+        console.log("match");
+        let epIndex = res.title.toLowerCase().indexOf("episode");
+        let epNo = res.title.slice(epIndex, epIndex + 10).trim();
+        console.log("ep no", epNo, res.title.toLowerCase().indexOf("episode"));
 
-  return;
+        chrome.bookmarks.getChildren("1", (bk) => {
+          let folderId = false;
 
-  await chrome.bookmarks.getChildren("1", (bk) => {
-    let folderId = false;
-
-    bk.forEach((element) => {
-      console.log(element.title, element.title === "My Shows Manager");
-      if (element.title === "My Shows Manager") {
-        folderId = element.id;
-      }
-    });
-
-    console.log("folderId", folderId);
-
-    if (!folderId) {
-      chrome.bookmarks.create(
-        {
-          parentId: "1",
-          title: "My Shows Manager",
-        },
-        (folder) => {
-          chrome.bookmarks.create({
-            parentId: folder.id,
-            url: res.url,
-            title: res.title,
+          bk.forEach((element) => {
+            console.log(element.title, element.title === "My Shows Manager");
+            if (element.title === "My Shows Manager") {
+              folderId = element.id;
+            }
           });
-        }
-      );
-    } else {
-      console.log("fired");
-      chrome.bookmarks.create({
-        parentId: folderId,
-        url: res.url,
-        title: res.title,
-      });
-    }
+
+          console.log("folderId", folderId);
+
+          if (!folderId) {
+            chrome.bookmarks.create(
+              {
+                parentId: "1",
+                title: "My Shows Manager",
+              },
+              (folder) => {
+                chrome.bookmarks.create({
+                  parentId: folder.id,
+                  url: res.url,
+                  title: show.title + " " + epNo,
+                });
+              }
+            );
+          } else {
+            console.log("fired");
+            chrome.bookmarks.create({
+              parentId: folderId,
+              url: res.url,
+              title: show.title + " " + epNo,
+            });
+          }
+        });
+      }
+    });
   });
 });
