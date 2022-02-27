05ae027bbfe4aab831f34621f10bddd3510dcc92
Ehsan <ehsanexe01@gmail.com>
2022-02-27 16:34:37 +0500

    bug fix

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.history.onVisited.addListener(async (res) => {
-  console.log("---", res, res.title);
+  console.log("res url , res title", res.url, res.title);
 
   chrome.storage.sync.get(["shows"], function (result) {
     // console.log("Value currently is " + result.shows);
@@ -8,21 +8,29 @@ chrome.history.onVisited.addListener(async (res) => {
     shows.forEach((show) => {
       let url = res.url.replace(/-/g, " ");
       console.log(
-        "url",
+        "clean url",
         url,
+        "show title",
         show.title,
+        "url includes show title",
         url.includes(show.title.toLowerCase())
       );
       if (
         (res.title.toLowerCase().includes(show.title.toLowerCase()) ||
-          url.includes(show.title.toLowerCase())) &&
-        res.title.toLowerCase().includes("episode") &&
+          url.toLowerCase().includes(show.title.toLowerCase())) &&
+        (res.title.toLowerCase().includes("episode") ||
+          url.toLowerCase().includes("episode")) &&
         !res.title.toLowerCase().includes("youtube")
       ) {
         console.log("match");
         let epIndex = res.title.toLowerCase().indexOf("episode");
+
+        if (epIndex === -1) {
+          epIndex = url.toLowerCase().indexOf("episode");
+        }
+
         let epNo = res.title.slice(epIndex, epIndex + 10).trim();
-        console.log("ep no", epNo, res.title.toLowerCase().indexOf("episode"));
+        console.log("ep no", epNo);
 
         chrome.bookmarks.getChildren("1", (bk) => {
           let folderId = false;
@@ -51,18 +59,18 @@ chrome.history.onVisited.addListener(async (res) => {
               }
             );
           } else {
-            console.log("else fired");
+            // console.log("else fired");
             chrome.bookmarks.getChildren(folderId, (showsBk) => {
-              console.log("shows bk", showsBk);
+              // console.log("shows bk", showsBk);
 
               showsBk.forEach((b) => {
                 let searchWord = b.title.slice(0, -10).trim();
-                console.log(
-                  "ddd",
-                  searchWord,
-                  show.title,
-                  show.title.toLowerCase().includes(searchWord.toLowerCase())
-                );
+                // console.log(
+                //   "ddd",
+                //   searchWord,
+                //   show.title,
+                //   show.title.toLowerCase().includes(searchWord.toLowerCase())
+                // );
                 if (
                   show.title.toLowerCase().includes(searchWord.toLowerCase())
                 ) {
